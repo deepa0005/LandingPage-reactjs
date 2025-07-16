34f4698768dc478ad7ca6f82e380e6b2caf7cdc8
deepa_kashyap <deepa494671@gmail.com>
2025-07-16 12:47:32 +0530

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,7 +65,7 @@ const Contact = () => {
 
 
     try {
-      const res = await fetch("https://landing-page-nodejs-1.onrender.com/api/zoho/send-leads", {
+      const res = await fetch("https://landing-page-nodejs-1.onrender.com/api/zoho/send-lead", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
